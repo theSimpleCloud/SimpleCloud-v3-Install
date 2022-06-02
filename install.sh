b6786759d7fc045fa7946c0f551e365d160b7693
Frederick Baier <baier.frederick@gmail.com>
2022-06-02 18:28:44 +0200

    Apply kubernetes configurations

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ apt install snapd
 snap install microk8s --classic
 microk8s status --wait-ready
 microk8s enable dns host-storage registry
+microk8s kubectl apply -f https://raw.githubusercontent.com/theSimpleCloud/SimpleCloud-v3-Install/main/content-server-pod.yml
+microk8s kubectl apply -f https://raw.githubusercontent.com/theSimpleCloud/SimpleCloud-v3-Install/main/proxy-service.yml
+microk8s kubectl apply -f https://raw.githubusercontent.com/theSimpleCloud/SimpleCloud-v3-Install/main/simplecloud-pod.yml
