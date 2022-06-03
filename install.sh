d993a189d1aaf4242034ab60f97dd2af8518d235
Frederick Baier <baier.frederick@gmail.com>
2022-06-03 22:01:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt update
 apt install snapd
 snap install microk8s --classic
 microk8s status --wait-ready
-microk8s enable dns host-storage registry
+microk8s enable dns hostpath-storage registry
 microk8s kubectl apply -f https://raw.githubusercontent.com/theSimpleCloud/SimpleCloud-v3-Install/main/content-server-pod.yml
 microk8s kubectl apply -f https://raw.githubusercontent.com/theSimpleCloud/SimpleCloud-v3-Install/main/proxy-service.yml
 microk8s kubectl apply -f https://raw.githubusercontent.com/theSimpleCloud/SimpleCloud-v3-Install/main/simplecloud-pod.yml
